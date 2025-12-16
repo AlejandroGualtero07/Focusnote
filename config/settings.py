5d00c41bd8c908134a6f73fe1ec637099bff6c13
Alejandro <167113394+AlejandroGualtero07@users.noreply.github.com>
2025-12-15 21:05:12 -0500

    Fix InvalidStorageError: Added 'default' FileSystemStorage to settings.STORAGES

--- config/settings.py
+++ config/settings.py
@@ -144,6 +144,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # WhiteNoise configuration
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
